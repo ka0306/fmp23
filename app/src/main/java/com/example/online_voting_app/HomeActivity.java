4be963ccf04222a4be620d3e592f19264c280f74
ka0306 <kashiimirani03@gmail.com>
2023-08-17 15:40:26 +0500

    3 videos done

---
+++ app/src/main/java/com/example/online_voting_app/HomeActivity.java
@@ -0,0 +1,14 @@
+package com.example.online_voting_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
--- app/src/main/java/com/example/online_voting_app/SignUp_activity.java
+++ app/src/main/java/com/example/online_voting_app/SignUp_activity.java
@@ -6,6 +6,8 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
@@ -22,6 +24,8 @@ import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.ktx.Firebase;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -30,20 +34,30 @@ public class SignUp_activity extends AppCompatActivity {
     private EditText userName,userPassword,userEmail,userNationalID;
     private Button singUpBtn;
     private FirebaseAuth mAuth;
+    public static final String PREFERENCE ="prefKey";
+    public static final String Name="nameKey";
+    public static final String Email="emailKey";
+    public static final String Password="passwordKey";
+    public static final String NationalId="nationalIdKey";
 
+    public static final String Image="imageKey";
+
+    SharedPreferences sharedPreferences;
+    String name,password,email,nationalId;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        sharedPreferences= getApplicationContext().getSharedPreferences(PREFERENCE,MODE_PRIVATE);
         findViewById(R.id.have_acc).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 onBackPressed();
             }
         });
-
         userProfile=findViewById(R.id.profile_image);
         userName=findViewById(R.id.user_name);
         userPassword=findViewById(R.id.user_password);
@@ -74,22 +88,21 @@ public class SignUp_activity extends AppCompatActivity {
         singUpBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String name=userName.getText().toString().trim();
-                String password=userPassword.getText().toString().trim();
-                String email=userEmail.getText().toString().trim();
-                String nationalId=userNationalID.getText().toString().trim();
+                 name=userName.getText().toString().trim();
+                 password=userPassword.getText().toString().trim();
+                 email=userEmail.getText().toString().trim();
+                 nationalId=userNationalID.getText().toString().trim();
 
                 if(!TextUtils.isEmpty(name)&&!TextUtils.isEmpty(password)
                         &&!TextUtils.isEmpty(email)&&Patterns.EMAIL_ADDRESS.matcher(email).matches()
                         &&!TextUtils.isEmpty(nationalId)){
                     createUser(email,password);
                 }else {
-                    Toast.makeText(SignUp_activity.this, "Please enter your credentials", Toast.LENGTH_SHORT).show();
+                Toast.makeText(SignUp_activity.this, "Please enter your credentials", Toast.LENGTH_SHORT).show();
                 }
             }
         });
-
-}
+     }
 
     private void createUser(String email, String password) {
         mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
@@ -97,10 +110,9 @@ public class SignUp_activity extends AppCompatActivity {
             public void onComplete(@NonNull Task<AuthResult> task) {
            if(task.isSuccessful()){
                Toast.makeText(SignUp_activity.this, " User created", Toast.LENGTH_SHORT).show();
-
+               verifyEmail();
            }else{
                Toast.makeText(SignUp_activity.this, "Fail Try again", Toast.LENGTH_SHORT).show();
-
            }
         }
         }).addOnFailureListener(new OnFailureListener() {
@@ -110,6 +122,37 @@ public class SignUp_activity extends AppCompatActivity {
 
             }
         });
+    }
+    private void verifyEmail() {
+        FirebaseUser user=mAuth.getCurrentUser();
+        if(user !=null){
+            user.sendEmailVerification().addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+
+                    if(task.isSuccessful()){
+                        SharedPreferences.Editor pref =sharedPreferences.edit();
+                        pref.putString(Name,name);
+                        pref.putString(Password,password);
+                        pref.putString(Email,email);
+                        pref.putString(NationalId,nationalId);
+                        pref.apply();
+//                      pref.putString(Image,  .toString());
+
+//                        email send
+                        Toast.makeText(SignUp_activity.this, "Email Send", Toast.LENGTH_SHORT).show();
+                        FirebaseAuth.getInstance().signOut();
+                        startActivity(new Intent(SignUp_activity.this, login_activity.class));
+                        finish();
+
+                    }else{
+                        mAuth.signOut();
+                        finish();
+                    }
+                }
+            });
+
+        }
     }
 
     private void cropImage() {
